009b98d48b2950200dd6ad887b875ec7c0ccfc21
jithin-05617 <30718811+jithin-05617@users.noreply.github.com>
2017-08-13 15:56:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req,res) {
-    res.send('hi i am jithin');
+res.sendFile(path.join(__dirname, 'ui', 'artilce-one.html'));
 });
 
 app.get('/article-two', function (req,res) {
